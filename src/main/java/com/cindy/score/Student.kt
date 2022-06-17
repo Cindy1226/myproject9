e91e1378202947a95a16b73c18e783bebe2474d8
cindy <cindywawa91@gmail.com>
2022-06-17 15:07:15 +0800

    6/17 second commit(student)

--- src/main/java/com/cindy/score/Student.kt
+++ src/main/java/com/cindy/score/Student.kt
@@ -1,10 +1,22 @@
 package com.cindy.com.cindy.score
 
-class Student {
-    var name = ""
-    var english =0
-    var math = 0
+class Student (var name:String, var english:Int, var math:Int){
+//    var name = ""
+//    var english =0
+//    var math = 0
+      init {
+//        println("testing")
+    }
+    fun print(){
+//        println(name+"\t")
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage () :Int {
+        return (english+math)/2
+    }
 }
 fun main (){
     val stu = Student("jack ",55,87)
+    stu.print()
+
 }
